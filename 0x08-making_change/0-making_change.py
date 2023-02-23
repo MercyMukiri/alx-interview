2a4567b69df462ebf9282d281e5991403bd8f6e6
Mercy Mukiri <mwmukiri@gmail.com>
2023-02-23 10:11:47 -0800

    Add project files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Determines the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Function that takes a list of the values of the coins in possession
+    and returns the fewest number of coins needed to meet the total
+    """
+    if total <= 0:
+        return 0
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+
+        if total == 0:
+            return counter
+
+        return -1
+
+                
