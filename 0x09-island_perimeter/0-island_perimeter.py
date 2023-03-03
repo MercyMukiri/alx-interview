c7307aaa6bca0a7f50d85f034a4d06e793e66fe0
Mercy Mukiri <mwmukiri@gmail.com>
2023-03-02 16:22:23 -0800

    Edit file 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,14 +11,15 @@ def island_perimeter(grid):
     perimeter = 0
 
     for i in range(len(grid)):
-        for j in range(len(grid)):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
-
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
+        for j in range(len(grid[0])):
+            perimeter += 4 * grid[i][j]
+            if i > 0:
+                perimeter -= grid[i][j] * grid[i-1][j]
+            if i < (len(grid)-1):
+                perimeter -= grid[i][j] * grid[i+1][j]
+            if j > 0:
+                perimeter -= grid[i][j] * grid[i][j-1]
+            if j < (len(grid[0])-1):
+                perimeter -= grid[i][j] * grid[i][j+1]
 
     return perimeter
